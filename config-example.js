fb78958dd48d4c6e5939fd4413350d239a318e38
ATAStaff <35782609+ATAStaff@users.noreply.github.com>
2018-01-24 18:50:45 -0500

    Update config-example.js

--- config-example.js
+++ config-example.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'NDA1NzE1Mzk4MTE0NDEwNDk2.DUqacQ.SU4HUni6WY-YWPwspbr7eIcUpiM',
+    token: 'NDA1NzE1Mzk4MTE0NDEwNDk2.DUqs4Q.5wdlmc-pzQ4X0-5hs3xXItPChE0',
     prefix: 'm.',
     ownerID: '111291959879983104'
 }
