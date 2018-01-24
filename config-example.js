4090ba356194d137c82cadfc71a0a08d723dbadc
ATAStaff <35782609+ATAStaff@users.noreply.github.com>
2018-01-24 18:30:53 -0500

    Update config-example.js

--- config-example.js
+++ config-example.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'p.LEasee-nt-eRYouRt_o.KENhe-Re',
-    prefix: 'b.',
-    ownerID: '1234567890'
+    token: 'NDA1NzE1Mzk4MTE0NDEwNDk2.DUqacQ.SU4HUni6WY-YWPwspbr7eIcUpiM',
+    prefix: 'm.',
+    ownerID: '111291959879983104'
 }
